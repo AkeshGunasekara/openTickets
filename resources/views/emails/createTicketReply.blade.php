a873e48ce6e7aa1593abdbcba543d6a39c680b72
AkeshGunasekara <akeshgunasekara@yahoo.com>
2022-12-10 10:16:27 +0530

    added text changes

--- resources/views/emails/createTicketReply.blade.php
+++ resources/views/emails/createTicketReply.blade.php
@@ -6,7 +6,7 @@
 <body>
     <h1>{{ $mailData['title'] }}</h1>
     <p>Ticket-ID : <span style="font-size: 2rem;">#{{ $mailData['ticketId'] }}</span></p>
-    <p>Response : <span style="font-size: 1rem;">#{{ $mailData['reply'] }}</span></p>
+    <p>Response : <span style="font-size: 1rem;">{{ $mailData['reply'] }}</span></p>
     <p>Thank you</p>
 </body>
 </html>
